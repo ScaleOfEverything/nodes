f542565993020efaf4e374fec421878cee422acc
Dave Caruso <dave@davecode.net>
2022-07-22 00:13:17 -0400

    fix: prod build.json categories

--- build/index.js
+++ build/index.js
@@ -4,18 +4,23 @@ import { DIST_ROOT, ROOT } from "./lib/paths.js";
 import { processNodeCategory } from "./lib/process.js";
 import { writeJSON } from "./lib/fs.js";
 
-const categories = (await readdir(path.join(ROOT, "build", "category"))).map(
-  (x) => x.replace(".js", "")
+const categories = await Promise.all(
+  (await readdir(path.join(ROOT, "build", "category")))
+    .map((x) => x.replace(".js", ""))
+    .map(async (c) => (await import(`./category/${c}.js`)).default)
 );
 
 for (const c of categories) {
-  await processNodeCategory((await import(`./category/${c}.js`)).default);
+  await processNodeCategory(c);
 }
 
 const build = {
   id: process.env.CF_PAGES_COMMIT_SHA || "dev",
   timestamp: new Date().toISOString(),
-  categories,
+  categories: categories.map((c) => ({
+    name: c.name,
+    projectURL: c.projectURL,
+  })),
 };
 
 await writeJSON(path.join(DIST_ROOT, "build.json"), build, {
